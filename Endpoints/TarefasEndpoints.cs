31077a26b05c565b73fbfd2d19413e7d3bff27b0
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2023-03-20 12:24:56 -0300

    Adicionar arquivos de projeto.

---
+++ Endpoints/TarefasEndpoints.cs
@@ -0,0 +1,58 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.VisualBasic.FileIO;
+using TarefasAPI.Data;
+using static TarefasAPI.Data.TarefaContext;
+
+namespace TarefasAPI.Endpoints;
+
+public static class TarefasEndpoints
+{
+    public static void MapTarefasEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", () => $"Bem vindo a API Tarefas {DateTime.Now}");
+
+        app.MapGet("/tarefas", async(GetConnection connectionGetter) =>
+        {
+            using var con = await connectionGetter();
+            var tarefas = con.GetAll<Tarefas>().ToList();
+            if (tarefas is null)
+                return Results.NotFound();
+
+            return Results.Ok(tarefas);
+        });
+        
+        app.MapGet("/tarefas/{id}", async(GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            return con.Get<Tarefas>(id) is Tarefas tarefa ? Results.Ok(tarefa) : Results.NotFound();
+        });
+        
+        app.MapPost("/tarefas", async(GetConnection connectionGetter, Tarefas Tarefa) =>
+        {
+            using var con = await connectionGetter();
+            var id = con.Insert(Tarefa);
+
+            return Results.Created($"/tarefas/{id}", Tarefa);
+        });
+        
+        app.MapPut("/tarefas", async(GetConnection connectionGetter, Tarefas Tarefa) =>
+        {
+            using var con = await connectionGetter();
+            var id = con.Update(Tarefa);
+
+            return Results.Ok();
+        });
+        
+        app.MapDelete("/tarefas/{id}", async(GetConnection connectionGetter, int id) =>
+        {
+            using var con = await connectionGetter();
+            var deleted = con.Get<Tarefas>(id);
+
+            if (deleted is null)
+                return Results.NotFound();
+
+            con.Delete(deleted);
+            return Results.Ok(deleted);
+        });
+    }
+}
